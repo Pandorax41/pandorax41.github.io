d609e30e5f1bf303ba8e60d140e055131b95a66d
pandorax41 <giunaulawilliam@gmail.com>
2021-06-18 12:04:17 -0500

    couple of changes

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210610.2212';
+const cacheName = 'chirpy-20210618.1203';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
